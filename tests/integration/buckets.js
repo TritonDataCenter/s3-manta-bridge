8425f1a84ece3886d96951ac0b4ce2844fae4228
Elijah Zupancic <elijahz@snapskout.com>
2016-03-23 19:31:21 -0700

    Added basic draft of test suite.

--- tests/integration/buckets.js
+++ tests/integration/buckets.js
@@ -1,38 +1,68 @@
 "use strict";
 
 var helper = require('./helper');
-var tape = require('tape');
+var test = require('tape');
 var vasync = require('vasync');
+/** @type {MantaClient} */
+var manta = require('../../lib/manta_client').client();
+var AWS = require('aws-sdk');
+var proxy = require('proxy-agent');
+var once = require('once');
+var uuid = require('node-uuid');
+var sync = require('sync');
 
 ///--- Globals
 
-function test(name, testRun) {
-    helper.createServer(function() {
-        vasync.pipeline({
-            funcs: [
-                function run(_, cb) {
-                    tape.test(name, testRun);
-                    cb();
-                },
-                function cleanup(_, cb) {
-                    helper.cleanupServer(function() {});
-                    cb();
-                }
-            ],
-            arg: null
-        }, function (err) {
-            if (err) {
-                throw err;
-            }
-        });
+AWS.config.update({
+    httpOptions: { agent: proxy('http://localhost:8888') },
+    sslEnabled: false,
+    credentials: new AWS.Credentials('', '', null)
+});
+
+var s3 = new AWS.S3();
+
+/////--- Server setup
+
+test.onFinish(helper.cleanup);
+
+test('setup', function(t) {
+    helper.startup(function () {
+        t.end();
     });
-}
+});
 
 /////--- Tests
 
-test('test-test', function(t) {
+test('can list buckets', function (t) {
     var log = helper.createLogger(t.name);
 
-    log.info("hello");
-    t.end();
+    vasync.pipeline({
+        'funcs': [
+            function (_, cb) {
+                log.debug("Adding test bucket");
+                var bucket1 = helper.config.bucketPath + '/test-' + uuid.v4();
+
+                manta.mkdirp(bucket1, function (err) {
+                    t.ifError(err);
+                });
+
+                cb();
+            },
+            function () {
+                setTimeout(function () {
+                    s3.listBuckets(function(err, data) {
+                        t.ifError(err);
+
+                        for (var index in data.Buckets) {
+                            var bucket = data.Buckets[index];
+                            log.debug("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+                        }
+
+                        t.end();
+                    });
+                }, 5000);
+
+            }
+        ]
+    });
 });
--- tests/integration/helper.js
+++ tests/integration/helper.js
@@ -1,9 +1,19 @@
 "use strict";
 
+var assert = require('assert-plus');
 var bunyan = require('bunyan');
 var app = require('../../lib');
-var config = require(process.cwd() + '/etc/config.json');
 var clone = require('clone');
+var uuid = require('node-uuid');
+var vasync = require('vasync');
+/** @type {MantaClient} */
+var manta = require('../../lib/manta_client').client();
+
+var config = {
+    "serverPort": 8080,
+    "prettyPrint": true,
+    "bucketPath": "~~/stor/s3_bridge_test/" + uuid.v4()
+};
 
 var server;
 
@@ -24,23 +34,49 @@ function createServer(cb) {
     opts.log = createLogger("rest-api");
 
     server = app.createServer(opts);
-    server.listen(opts.serverPort, function () {
+
+    server.once('listening', function restifyListening() {
         opts.log.info('%s listening at %s', server.name, server.url);
-        cb();
+
+        if (cb) {
+            cb(server);
+        }
     });
+
+    server.listen(opts.serverPort);
 }
 
 function cleanupServer(cb) {
+    server.log.info("Cleaning up server");
     if (server) {
         server.close();
-    } else {
+    }
+
+    if (cb) {
         cb();
     }
 }
 
+function startup(cb) {
+    createServer(cb);
+}
+
+function cleanup(cb) {
+    assert.object(config);
+    var log = createLogger("cleanup");
+
+    log.debug('Cleaning up test Manta files')
+    manta.rmr(config.bucketPath, {}, function (err) {
+        // do nothing
+    });
+
+    cleanupServer(cb);
+}
+
 module.exports = {
+    config: config,
     createLogger: createLogger,
     //createClient: createClient,
-    createServer: createServer,
-    cleanupServer: cleanupServer
+    startup: createServer,
+    cleanup: cleanup
 };

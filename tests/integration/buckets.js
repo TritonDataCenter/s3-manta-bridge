88b29497d375245daab3d1f7065e0d8813f0cebd
Elijah Zupancic <elijahz@snapskout.com>
2016-03-25 09:08:38 -0700

    Added working implementation of integration tests that rely on external server.

--- tests/integration/buckets.js
+++ tests/integration/buckets.js
@@ -1,17 +1,22 @@
 "use strict";
 
+var request = require('sync-request');
 var helper = require('./helper');
-var test = require('tape');
 var vasync = require('vasync');
 /** @type {MantaClient} */
-var manta = require('../../lib/manta_client').client();
 var AWS = require('aws-sdk');
 var proxy = require('proxy-agent');
-var once = require('once');
 var uuid = require('node-uuid');
-var sync = require('sync');
+var util = require('util');
+
+var test = require('wrapping-tape')({
+    setup: helper.startup,
+    teardown: helper.cleanup
+});
 
 ///--- Globals
+
+var manta = helper.mantaClient;
 
 AWS.config.update({
     httpOptions: { agent: proxy('http://localhost:8888') },
@@ -21,47 +26,49 @@ AWS.config.update({
 
 var s3 = new AWS.S3();
 
-/////--- Server setup
+/////--- Tests
 
-test.onFinish(helper.cleanup);
+test('server is alive', function (t) {
+    t.plan(1);
+    var port = helper.config.serverPort;
+    var host = 'http://localhost:' + port;
+    var res = request('HEAD', host);
 
-test('setup', function(t) {
-    helper.startup(function () {
-        t.end();
+    t.equal(res.statusCode, 200, "Expecting server to be reachable at " + host);
+});
+
+test('can list buckets - no buckets', function (t) {
+    t.plan(2);
+
+    s3.listBuckets(function(err, data) {
+        t.ifError(err, 'Expecting S3 buckets list call to succeed');
+
+        var buckets = data.Buckets;
+        t.equal(buckets.length, 0, 'Expecting no buckets to return');
     });
 });
 
-/////--- Tests
-
-test('can list buckets', function (t) {
-    var log = helper.createLogger(t.name);
+test('can list buckets - one bucket', function (t) {
+    var newBucket = 'test-' + uuid.v4();
+    var newBucketPath = helper.config.bucketPath + '/' + newBucket;
 
     vasync.pipeline({
         'funcs': [
-            function (_, cb) {
-                log.debug("Adding test bucket");
-                var bucket1 = helper.config.bucketPath + '/test-' + uuid.v4();
-
-                manta.mkdirp(bucket1, function (err) {
-                    t.ifError(err);
+            function (_, next) {
+                manta.mkdirp(newBucketPath, function (err) {
+                    t.ifError(err, 'Expecting mkdirp to succeed for ' + newBucket);
+                    next();
                 });
-
-                cb();
             },
-            function () {
-                setTimeout(function () {
-                    s3.listBuckets(function(err, data) {
-                        t.ifError(err);
+            function (_) {
+                s3.listBuckets(function (err, data) {
+                    t.ifError(err, 'Expecting S3 buckets list call to succeed');
 
-                        for (var index in data.Buckets) {
-                            var bucket = data.Buckets[index];
-                            log.debug("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
-                        }
-
-                        t.end();
-                    });
-                }, 5000);
-
+                    var buckets = data.Buckets;
+                    t.equal(buckets.length, 1, 'Only a single bucket expected');
+                    t.equal(buckets[0].Name, newBucket, 'Bucket is named: ' + newBucket);
+                    t.end();
+                });
             }
         ]
     });
--- tests/integration/helper.js
+++ tests/integration/helper.js
@@ -2,20 +2,11 @@
 
 var assert = require('assert-plus');
 var bunyan = require('bunyan');
-var app = require('../../lib');
-var clone = require('clone');
 var uuid = require('node-uuid');
-var vasync = require('vasync');
+var config = require('../../etc/config.json');
+
 /** @type {MantaClient} */
 var manta = require('../../lib/manta_client').client();
-
-var config = {
-    "serverPort": 8080,
-    "prettyPrint": true,
-    "bucketPath": "~~/stor/s3_bridge_test/" + uuid.v4()
-};
-
-var server;
 
 function createLogger(name, stream) {
     var log = bunyan.createLogger({
@@ -29,54 +20,29 @@ function createLogger(name, stream) {
     return log;
 }
 
-function createServer(cb) {
-    var opts = clone(config);
-    opts.log = createLogger("rest-api");
-
-    server = app.createServer(opts);
-
-    server.once('listening', function restifyListening() {
-        opts.log.info('%s listening at %s', server.name, server.url);
-
-        if (cb) {
-            cb(server);
-        }
-    });
-
-    server.listen(opts.serverPort);
-}
-
-function cleanupServer(cb) {
-    server.log.info("Cleaning up server");
-    if (server) {
-        server.close();
-    }
-
-    if (cb) {
-        cb();
-    }
-}
-
-function startup(cb) {
-    createServer(cb);
-}
-
-function cleanup(cb) {
+function startup(tape) {
     assert.object(config);
-    var log = createLogger("cleanup");
 
-    log.debug('Cleaning up test Manta files')
+    manta.mkdirp(config.bucketPath, function (err) {
+        tape.ifError(err, 'Creating bucket path: ' + config.bucketPath);
+        tape.end();
+    });
+}
+
+function cleanup(tape) {
+    assert.object(config);
     manta.rmr(config.bucketPath, {}, function (err) {
         // do nothing
-    });
 
-    cleanupServer(cb);
+        tape.ifError(err, 'Cleaning up test Manta files: ' + config.bucketPath);
+        tape.end();
+    });
 }
 
 module.exports = {
     config: config,
     createLogger: createLogger,
-    //createClient: createClient,
-    startup: createServer,
+    mantaClient: manta,
+    startup: startup,
     cleanup: cleanup
 };

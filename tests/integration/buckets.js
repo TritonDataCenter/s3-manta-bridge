b81ffcd262a9e66a67ceb4617400054ca3644db3
Elijah Zupancic <elijahz@snapskout.com>
2016-03-29 16:11:00 -0700

    Added create bucket support.

--- tests/integration/buckets.js
+++ tests/integration/buckets.js
@@ -25,85 +25,85 @@ test('server is alive', function (t) {
     t.equal(res.statusCode, 200, "Expecting server to be reachable at " + host);
 });
 
-//test('can list buckets - no buckets', function (t) {
-//    t.plan(2);
-//
-//    s3.listBuckets(function(err, data) {
-//        t.ifError(err, 'Expecting S3 buckets list call to succeed');
-//
-//        var buckets = data.Buckets;
-//        t.equal(buckets.length, 0, 'Expecting no buckets to return');
-//    });
-//});
-//
-//test('can list buckets - one bucket', function (t) {
-//    var newBucket = 'test-' + mod_uuid.v4();
-//    var newBucketPath = helper.config.bucketPath + '/' + newBucket;
-//
-//    mod_vasync.pipeline({
-//        'funcs': [
-//            function (_, next) {
-//                manta.mkdirp(newBucketPath, function (err) {
-//                    t.ifError(err, 'Expecting mkdirp to succeed for ' + newBucket);
-//                    next();
-//                });
-//            },
-//            function (_) {
-//                s3.listBuckets(function (err, data) {
-//                    t.ifError(err, 'Expecting S3 buckets list call to succeed');
-//
-//                    var buckets = data.Buckets;
-//                    t.equal(buckets.length, 1, 'Only a single bucket expected');
-//                    t.equal(buckets[0].Name, newBucket, 'Bucket is named: ' + newBucket);
-//                    t.end();
-//                });
-//            }
-//        ]
-//    });
-//});
-//
-//test('can list buckets - one thousand two hundred buckets', function (t) {
-//    // This number of buckets forces the Manta client to paginate
-//    var iterations = 1200;
-//
-//    var newBucketPrefix = helper.config.bucketPath + '/';
-//
-//    function mkdir(dir, cb) {
-//        manta.mkdir(dir, cb);
-//    }
-//
-//    mod_vasync.pipeline({
-//        'funcs': [
-//            function (_, next) {
-//                var queue = mod_vasync.queue(mkdir, 20);
-//
-//                queue.on('end', function() {
-//                    next();
-//                });
-//
-//                var mkdirErrHandle = function (err) {
-//                    mod_assert.ifError(err);
-//                };
-//
-//                var buckets = mod_lo.times(iterations, function() {
-//                    return newBucketPrefix + 'test-' + mod_uuid.v4();
-//                });
-//
-//                queue.push(buckets, mkdirErrHandle);
-//                queue.close();
-//            },
-//            function (_) {
-//                s3.listBuckets(function (err, data) {
-//                    t.ifError(err, 'Expecting S3 buckets list call to succeed');
-//
-//                    var buckets = data.Buckets;
-//                    t.equal(buckets.length, iterations, iterations + ' expected');
-//                    t.end();
-//                });
-//            }
-//        ]
-//    });
-//});
+test('can list buckets - no buckets', function (t) {
+   t.plan(2);
+
+   s3.listBuckets(function(err, data) {
+       t.ifError(err, 'Expecting S3 buckets list call to succeed');
+
+       var buckets = data.Buckets;
+       t.equal(buckets.length, 0, 'Expecting no buckets to return');
+   });
+});
+
+test('can list buckets - one bucket', function (t) {
+   var newBucket = 'test-' + mod_uuid.v4();
+   var newBucketPath = helper.config.bucketPath + '/' + newBucket;
+
+   mod_vasync.pipeline({
+       'funcs': [
+           function (_, next) {
+               manta.mkdirp(newBucketPath, function (err) {
+                   t.ifError(err, 'Expecting mkdirp to succeed for ' + newBucket);
+                   next();
+               });
+           },
+           function (_) {
+               s3.listBuckets(function (err, data) {
+                   t.ifError(err, 'Expecting S3 buckets list call to succeed');
+
+                   var buckets = data.Buckets;
+                   t.equal(buckets.length, 1, 'Only a single bucket expected');
+                   t.equal(buckets[0].Name, newBucket, 'Bucket is named: ' + newBucket);
+                   t.end();
+               });
+           }
+       ]
+   });
+});
+
+test('can list buckets - one thousand two hundred buckets', function (t) {
+   // This number of buckets forces the Manta client to paginate
+   var iterations = 1200;
+
+   var newBucketPrefix = helper.config.bucketPath + '/';
+
+   function mkdir(dir, cb) {
+       manta.mkdir(dir, cb);
+   }
+
+   mod_vasync.pipeline({
+       'funcs': [
+           function (_, next) {
+               var queue = mod_vasync.queue(mkdir, 20);
+
+               queue.on('end', function() {
+                   next();
+               });
+
+               var mkdirErrHandle = function (err) {
+                   mod_assert.ifError(err);
+               };
+
+               var buckets = mod_lo.times(iterations, function() {
+                   return newBucketPrefix + 'test-' + mod_uuid.v4();
+               });
+
+               queue.push(buckets, mkdirErrHandle);
+               queue.close();
+           },
+           function () {
+               s3.listBuckets(function (err, data) {
+                   t.ifError(err, 'Expecting S3 buckets list call to succeed');
+
+                   var buckets = data.Buckets;
+                   t.equal(buckets.length, iterations, iterations + ' expected');
+                   t.end();
+               });
+           }
+       ]
+   });
+});
 
 test('can add bucket', function(t) {
     var bucket = 'predictable-bucket-name';
--- tests/unit/utils.js
+++ tests/unit/utils.js
@@ -5,21 +5,21 @@ var test = require('tape');
 
 test('parseDomainFromHostWithPort - can parse a hostname without a port', function (t) {
     var hostname = 'subdomain.domain.tld';
-    var result = utils.parseHostname(hostname);
+    var result = utils.parseDomainFromHostWithPort(hostname);
     t.equal(result, hostname, 'the hostname without a port should be the same');
     t.end();
 });
 
 test('parseDomainFromHostWithPort - can parse a hostname with a port', function (t) {
     var hostname = 'subdomain.domain.tld:9977';
-    var result = utils.parseHostname(hostname);
+    var result = utils.parseDomainFromHostWithPort(hostname);
     t.equal(result, 'subdomain.domain.tld', 'the hostname with a port should be just the domain');
     t.end();
 });
 
 test('parseDomainFromHostWithPort - can deal with only a colon', function (t) {
     var hostname = ':';
-    var result = utils.parseHostname(hostname);
+    var result = utils.parseDomainFromHostWithPort(hostname);
     t.equal(result, '', 'if the input is so bad that we only have a colon, return an empty string');
     t.end();
 });

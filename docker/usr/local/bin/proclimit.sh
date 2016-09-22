a6cab314ea57823e0afe160f77f1bc8550d557e7
Elijah Zupancic <elijah@zupancic.name>
2016-09-22 16:03:40 -0700

    Updated proclimit script to the latest version.

--- docker/usr/local/bin/proclimit.sh
+++ docker/usr/local/bin/proclimit.sh
@@ -1,31 +1,54 @@
-#!/native/usr/bin/ksh93
+#!/usr/bin/env sh
 
-PATH=/usr/bin
-if [[ -d /native ]]; then
-    PATH=/native/usr/bin
+##
+# When this script is invoked inside of a zone:
+#
+# This script returns a number representing a very conservative estimate of the
+# maximum number of processes or threads that you want to run within the zone
+# that invoked this script. Typically, you would take this value and define a
+# multiplier that works well for your application.
+#
+# Otherwise:
+# This script returns the number of cores reported by the OS.
+
+# If we are on a LX Brand Zone calculation value using utilities only available in the /native
+# directory
+
+if [ -d /native ]; then
+  PATH=/native/sbin:/native/usr/bin:/native/sbin:$PATH
 fi
 
-set -o errexit
-if [[ -n ${TRACE} ]]; then
-    set -o xtrace
+KSH="$(which ksh93)"
+PRCTL="$(which prctl)"
+
+if [ -n "${KSH}" ] && [ -n "${PRCTL}" ]; then
+  CAP=$(${KSH} -c "echo \$((\$(${PRCTL} -n zone.cpu-cap \$\$ | grep privileged | awk '{ print \$2 }') / 100))")
+
+  # If there is no cap set, then we will fall through and use the other functions
+  # to determine the maximum processes.
+  if [ -n "${CAP}" ]; then
+    $KSH -c "echo \$((ceil(${CAP})))"
+    exit 0
+  fi
 fi
 
-# CN parameters
-CORES=$(kstat -C -m cpu_info -c misc -s core_id | wc -l | tr -d ' ')
-TOTAL_MEMORY=$(($(kstat -C -m unix -n system_pages -c pages -s physmem \
-    | cut -d':' -f5) * $(pagesize))).0
+# Linux calculation if you have nproc
+if [ -n "$(which nproc)" ]; then
+  nproc
+  exit 0
+fi
 
-# zone parameters
-ZONE_MEMORY=$(kstat -C -m memory_cap -c zone_memory_cap -s physcap \
-    | cut -d':' -f5).0
+# Linux more widely supported implementation
+if [ -f /proc/cpuinfo ] && [ -n $(which wc) ]; then
+  grep processor /proc/cpuinfo | wc -l
+  exit 0
+fi
 
-# our fraction of the total memory on the CN
-MEMORY_SHARE=$((${ZONE_MEMORY} / ${TOTAL_MEMORY}))
+# OS X calculation
+if [ "$(uname)" == "Darwin" ]; then
+  sysctl -n hw.ncpu
+  exit 0
+fi
 
-# best guess as to how many CPUs we should pretend like we have for tuning
-CPU_GUESS=$((${CORES} * ${MEMORY_SHARE}))
-
-# round that up to a positive integer
-echo "$((ceil(${CPU_GUESS})))"
-
-exit 0
+# Fallback value if we can't calculate
+echo 1
